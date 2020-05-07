15d3b975ae8af50c514bc7a8b9aef70ece283158
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-07 17:16:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function findMatching(array, str) {
   return array.filter(element => {return element.toLowerCase() === str.toLowerCase();});
 }
 
-function fuzzmatch(array, str) {
+function fuzzyMatch(array, str) {
   return array.filter(element => {return element.toLowerCase().indexOf(str.toLowerCase()) === 0});
 }
