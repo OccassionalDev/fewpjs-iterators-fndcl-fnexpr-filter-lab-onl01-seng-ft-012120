544cfeac914b50bccfbcd1800d7da96b55a39986
Erica Schimmel <ericaschimmel01@gmail.com>
2020-05-07 17:14:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 function findMatching(array, str) {
   return array.filter(element => {return element.toLowerCase() === str.toLowerCase();});
 }
+
+function fuzzmatch(array, str) {
+  return array.filter(element => {return element === });
+}
